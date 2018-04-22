87b2bf568203181d4012a18a9677acd68ac04844
lokesh-venkata-sai <mamidil.sai@st.niituniversity.in>
2018-04-22 15:45:49 +0530

    corrected spinner

--- app/src/main/java/com/example/lokesh/bloodline/profile.java
+++ app/src/main/java/com/example/lokesh/bloodline/profile.java
@@ -5,13 +5,15 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class profile extends AppCompatActivity implements View.OnClickListener{
 
     private Button back_prof;
-
+    private Spinner bloodgroup_prof;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,11 @@ public class profile extends AppCompatActivity implements View.OnClickListener{
         back_prof=(Button) findViewById(R.id.back_prof);
         back_prof.setOnClickListener(this);
 
-
+        Spinner bloodgroup_prof=(Spinner) findViewById(R.id.bloodgroup_prof);
+        ArrayAdapter<String> myadapter=new ArrayAdapter<String>(profile.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.registerpage_bloodgroup));
+        myadapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        bloodgroup_prof.setAdapter(myadapter);
 
     }
 
